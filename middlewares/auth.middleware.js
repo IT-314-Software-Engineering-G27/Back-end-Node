df767b22a21c21c00243423b89f5f2c229bfd0ac
sohamviradiya <sohamviradiya2003@gmail.com>
2023-10-26 10:45:56 +0530

    add auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+const { UnauthorizedError } = require("../errors");
+const { verifyToken } = require("../services/auth.service");
+
+async function tokenToIDMiddleware(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) 
+        throw new UnauthorizedError("No token provided");
+    
+    const authServiceResponse = await verifyToken({ token });
+    req.user = authServiceResponse.user;
+    next();
+};
+
+module.exports = {
+    tokenToIDMiddleware,
+};
