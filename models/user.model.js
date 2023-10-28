6f078df64543ca0a66fecaed3f47fcf5cc4f01ab
sohamviradiya <sohamviradiya2003@gmail.com>
2023-10-28 21:31:25 +0530

    expand user schema

--- models/user.model.js
+++ models/user.model.js
@@ -8,12 +8,61 @@ const UserSchema = new Schema({
         required: [true, "Email is required"],
         unique: true,
         maxlength: 255,
+        index: true,
     },
     password: {
         type: Schema.Types.String,
         required: [true, "Password is required"],
         minlength: 8,
         maxlength: 1024,
+    },
+    username: {
+        type: Schema.Types.String,
+        required: [true, "Username is required"],
+        unique: true,
+        minlength: [3, "Username is too short"],
+        maxlength: [255, "Username is too long"],
+        index: true,
+    },
+    role: {
+        type: Schema.Types.String,
+        enum: ["individual", "organization"],
+        default: "individual",
+    },
+    individual: {
+        type: Schema.Types.ObjectId,
+        ref: "Individual",
+    },
+    organization: {
+        type: Schema.Types.ObjectId,
+        ref: "Organization",
+    },
+    phone_number: {
+        type: Schema.Types.String,
+    },
+    profile_image: {
+        type: Schema.Types.String,
+    },
+    posts: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: "Post",
+        }],
+        default: [],
+    },
+    reports: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }],
+        default: [],
+    },
+    connections: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: "Connection",
+        }],
+        default: [],
     },
 });
 
