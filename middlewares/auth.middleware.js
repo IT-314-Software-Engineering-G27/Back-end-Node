c768a208dcaba440612be76f5d710feaa4fd7792
sohamviradiya <sohamviradiya2003@gmail.com>
2023-10-30 15:10:58 +0530

    handle no token error

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,12 +2,18 @@ const { UnauthorizedError } = require("../errors");
 const { verifyToken } = require("../services/auth.service");
 
 async function tokenToIDMiddleware(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token)
-        throw new UnauthorizedError("No token provided");
-    const authServiceResponse = await verifyToken({ token });
-    req.user = authServiceResponse.user;
-    next();
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            throw new UnauthorizedError("No token provided");
+        }
+        const authServiceResponse = await verifyToken({ token });
+        req.user = authServiceResponse.user;
+        next();
+    }
+    catch (error) {
+        next(error);
+    }
 };
 
 module.exports = {
