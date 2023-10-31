0d2ee0f4a89067d138321903cb1cc349d38e46c5
sohamviradiya <sohamviradiya2003@gmail.com>
2023-10-31 13:43:13 +0530

    add job-apply service

--- controllers/jobProfile.controller.js
+++ controllers/jobProfile.controller.js
@@ -1,7 +1,9 @@
 const LIMIT_PER_PAGE = 10;
 const { getJobProfile } = require("../services/jobProfile.service");
 const { listJobProfiles, listApplications, deleteJobProfile, createJobProfile, getJobProfileBasic, updateJobProfile, } = require("../services/jobProfile.service");
+const { createJobApplication } = require("../services/jobApplication.service");
 const { transformInputToJobProfile } = require("../services/utils/jobProfile.util");
+const { ForbiddenError, BadRequestError } = require("../errors");
 
 const JobProfileController = {
     list: async (req, res, next) => {
@@ -20,12 +22,14 @@ const JobProfileController = {
     },
     create: async (req, res, next) => {
         try {
+            if(req.user.role !== 'organization') throw new ForbiddenError('You are not authorized to access this resource');
             const { jobProfile } = req.body;
+            if(!jobProfile) throw new BadRequestError('Job profile is required');
             const newJobProfile = await createJobProfile({ jobProfile: transformInputToJobProfile({ jobProfile }), organizationId: req.user.organization });
             res.json({
                 message: "Job profile created successfully",
                 payload: {
-                    profile: newJobProfile,
+                    jobProfile: newJobProfile,
                 }
             });
         } catch (error) {
@@ -71,14 +75,30 @@ const JobProfileController = {
             next(error);
         }
     },
+    apply: async (req, res, next) => {
+        try {
+            if(req.user.role !== 'individual') throw new ForbiddenError('You are not authorized to access this resource');
+            const { cover_letter } = req.body;
+            const newJobApplication = await createJobApplication({ jobApplication: { cover_letter: cover_letter ?? "" }, individualId: req.user.individual, jobProfileId: req.params.id });
+            res.json({
+                message: "Job application created successfully",
+                payload: {
+                    application: newJobApplication,
+                }
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
     update: async (req, res, next) => {
         try {
             const { jobProfile } = req.body;
-            const newJobProfile = await updateJobProfile({ jobProfileId: req.params.id, jobProfile: transformInputToJobProfile({ jobProfile: jobProfile }) });
+            if(!jobProfile) throw new BadRequestError('Job profile is required');
+            const newJobProfile = await updateJobProfile({ jobProfileId: req.params.id, jobProfile: transformInputToJobProfile({ jobProfile }) });
             res.json({
                 message: "Job profile updated successfully",
                 payload: {
-                    profile: newJobProfile,
+                    jobProfile: newJobProfile,
                 }
             });
         } catch (error) {
