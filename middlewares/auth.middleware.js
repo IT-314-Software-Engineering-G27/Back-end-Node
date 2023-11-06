1d75bf72e628ff83d94ac4bb3473508b9e378f0c
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-06 20:47:36 +0530

    resolve no auth errors

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,6 +3,9 @@ const { verifyToken } = require("../services/auth.service");
 
 async function tokenToIDMiddleware(req, res, next) {
     try {
+        if (!req.headers.authorization) {
+            throw new UnauthorizedError("Authorization header is required");
+        }
         const token = req.headers.authorization.split(" ")[1];
         if (!token) {
             throw new UnauthorizedError("No token provided");
