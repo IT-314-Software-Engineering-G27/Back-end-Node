823589b3efa53deafb9aee96069cc581166dc3ec
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-07 01:18:43 +0530

    update message middleware

--- services/message.service.js
+++ services/message.service.js
@@ -2,11 +2,11 @@ const ConnectionModel = require("../models/connection.model");
 const MessageModel = require("../models/message.model");
 
 async function createMessage({ role, connectionId, message: { content } }) {
-    const newMessage = await MessageModel.create({
+    const newMessage = (await MessageModel.create({
         connection: connectionId,
         direction: role == "from" ? "from-to" : "to-from",
         content,
-    });
+    })).toJSON();
     await ConnectionModel.findByIdAndUpdate(connectionId, {
         $push: { messages: newMessage._id },
     }).exec();
@@ -14,29 +14,29 @@ async function createMessage({ role, connectionId, message: { content } }) {
 };
 
 async function getMessage({ messageId }) {
-    return await MessageModel.findById(messageId).exec();
+    return (await MessageModel.findById(messageId).exec()).toJSON();
 }
 
 async function updateMessage({ messageId, content }) {
-    return await MessageModel.findByIdAndUpdate(messageId, {
+    return (await MessageModel.findByIdAndUpdate(messageId, {
         content,
         sent_timestamp: Date.now(),
-    }, { new: true }).exec();
+    }, { new: true }).exec()).toJSON();
 }
 
 async function getMessageBasic({ messageId }) {
-    return await MessageModel.findById(messageId, {
+    return (await MessageModel.findById(messageId, {
         connection: 1,
         direction: 1,
         status: 1,
-    });
+    })).toJSON();
 }
 
 async function readMessage({ messageId }) {
-    return await MessageModel.findByIdAndUpdate(messageId, {
+    return (await MessageModel.findByIdAndUpdate(messageId, {
         status: "read",
         read_timestamp: Date.now(),
-    }, { new: true }).exec();
+    }, { new: true }).exec()).toJSON();
 };
 
 async function deleteMessage({ messageId }) {
