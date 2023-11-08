d2cf062ba4b92bcfa83289c937e596bfda177b37
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-08 14:28:57 +0530

    update post services

--- services/post.service.js
+++ services/post.service.js
@@ -1,112 +1,137 @@
 const { BadRequestError, NotFoundError } = require('../errors');
 const PostModel = require('../models/post.model');
-const { uploadFile, removeFile } = require('./file.service');
+const UserModel = require('../models/user.model');
 
-async function createPost({ post, imageFile }) {
-    try {
-      if (imageFile) {
-        const imageFileId = await uploadFile(imageFile);
-        post.image = imageFileId; // Attach the image file's ID to the post
-      }
-  
-      return await PostModel.create(post);
-    } catch (error) {
-      throw new BadRequestError(error.message);
-    }
+async function listPosts({ query, page, limit }) {
+  return (await PostModel.find({
+    $or: [
+      { title: { $regex: `${query}`, $options: 'i', }, },
+    ]
+  }, {
+    _id: 1,
+  }).sort({ likes: -1 }).skip(page * limit).limit(limit).exec()).map((post) => post._id);
+};
+
+async function deepSearchPosts({ query, page, limit }) {
+  return (await PostModel.find({ $text: { $search: query } }, {
+    score: {
+      $meta: 'textScore',
+    },
+    _id: 1,
+  }).sort({
+    score: {
+      $meta: 'textScore',
+    },
+  }).skip(page * limit).limit(limit).exec()).map((post) => post._id);
+};
+
+async function createPost({ post, userId }) {
+  const newPost = await PostModel.create({
+    ...post,
+    user: userId,
+  });
+  await UserModel.findByIdAndUpdate(userId, { $push: { posts: newPost._id } }).exec();
+  return newPost;
 }
 
-async function updatePost({ postId, post, imageFile }) {
-    const updatedPost = await PostModel.findByIdAndUpdate(postId, post, {
-      new: true,
-    }).exec();
-    if (!updatedPost) {
-      throw new NotFoundError('Post not found');
-    }
-  
-    if (imageFile) {
-      if (updatedPost.image) {
-        await removeFile(updatedPost.image); // Remove the previous image
-      }
-      const imageFileId = await uploadFile(imageFile);
-      updatedPost.image = imageFileId; // Update the image file's ID
-    }
-  
-    return updatedPost;
+async function updatePostImage({ postId, fileId, host }) {
+  const url = `${host}/files/${fileId}`;
+  return await PostModel.findByIdAndUpdate(
+    postId,
+    { image: url },
+    { new: true }
+  ).exec();
 }
+
+async function updatePost({ postId, post }) {
+  const updatedPost = await PostModel.findByIdAndUpdate(postId, post, {
+    new: true,
+  }).exec();
+  if (!updatedPost)
+    throw new NotFoundError('Post not found');
+  return updatedPost;
+}
+
+async function getPostBasic({ postId }) {
+  return await PostModel.findById(postId, {
+    title: 1,
+    subject: 1,
+    user: 1,
+    time: 1,
+  }).populate({
+    path: 'user',
+    select: {
+      username: 1,
+      profile_image: 1,
+      role: 1,
+    }
+  }).exec();
+};
 
 async function getPost({ postId }) {
-    const post = await PostModel.findById(postId).exec();
-    if (!post) {
-      throw new NotFoundError('Post not found');
+  const post = await PostModel.findById(postId).populate({
+    path: 'user',
+    select: {
+      username: 1,
+      profile_image: 1,
+      role: 1,
+      individual: 1,
+      organization: 1,
     }
-    return post;
-}
-
-async function deletePost({ postId }) {
-  const post = await PostModel.findByIdAndDelete(postId).exec();
+  }).exec();
   if (!post) {
     throw new NotFoundError('Post not found');
   }
   return post;
-}
+};
 
-async function listPosts({ page, limit }) {
-  return PostModel.find({}, { _id: 1 })
-    .skip(page * limit)
-    .limit(limit)
-    .exec();
-}
+async function deletePost({ postId, userId }) {
+  await PostModel.findByIdAndDelete(postId).exec();
+  await UserModel.findByIdAndUpdate(userId, { $pull: { posts: postId } }).exec();
+  return true;
+};
 
-async function createLike({ postId, userId }) {
-    const post = await PostModel.findById(postId).exec();
-    if (!post) {
-      throw new NotFoundError('Post not found');
-    }
-  
-    if (!post.likes.includes(userId)) {
-      post.likes.push(userId);
-      await post.save();
-    }
-  
-    return post;
-}
+async function getPostStatus({ postId, userId }) {
+  const isLiked = await PostModel.findById(postId, {
+    likes: {
+      $elemMatch: {
+        $eq: userId,
+      },
+    },
+  });
+  return {
+    isLiked: isLiked.likes.length > 0,
+  };
+};
 
 async function likePost({ postId, userId }) {
-  const post = await PostModel.findById(postId).exec();
-  if (!post) {
-    throw new NotFoundError('Post not found');
-  }
-
-  if (!post.likes.includes(userId)) {
-    post.likes.push(userId);
-    await post.save();
-  }
-
-  return post;
-}
+  return await PostModel.findByIdAndUpdate(
+    postId,
+    { $addToSet: { likes: userId } },
+    { new: true }
+  ).exec();
+};
 
 async function unlikePost({ postId, userId }) {
-  const post = await PostModel.findById(postId).exec();
-  if (!post) {
-    throw new NotFoundError('Post not found');
-  }
+  return await PostModel.findByIdAndUpdate(
+    postId,
+    { $pull: { likes: userId } },
+    { new: true }
+  ).exec();
+};
 
-  const index = post.likes.indexOf(userId);
-  if (index !== -1) {
-    post.likes.splice(index, 1);
-    await post.save();
-  }
 
-  return post;
-}
 
 module.exports = {
   createPost,
   updatePost,
+  updatePostImage,
   getPost,
+  getPostBasic,
   deletePost,
   listPosts,
-  createLike,
+  deepSearchPosts,
+  getPostStatus,
   likePost,
   unlikePost,
 };
